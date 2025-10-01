93484f480b051506c03b71f9df84d231c3ba89da
Alex <G00424185@atu.ie>
2025-10-01 09:40:28 +0100

    Implemented changes based on review comments.

--- app/main.py
+++ app/main.py
@@ -9,28 +9,50 @@ users: list[User] = []
 def get_users():
     return users
 
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
+
+
 @app.get("/api/users/{user_id}")
 def get_user(user_id: int):
+    #loop users until an id matches
     for u in users:
         if u.user_id == user_id:
             return u
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
+
 @app.post("/api/users", status_code=status.HTTP_201_CREATED)
 def add_user(user: User):
+    #checks the user object id matches for any object
     if any(u.user_id == user.user_id for u in users):
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="user_id already exists")
     users.append(user)
     return user
 
-@app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(user_id: int):
-    for u in users:
+
+@app.put("/api/users/{user_id}")
+def edit_user(user_id: int, user: User):
+    #Compare the object at each index to see matching id
+    for i, u in enumerate(users):
         if u.user_id == user_id:
+            #Once they match change the attributes at the index
+            users[i].name = user.name
+            users[i].email = user.email
+            users[i].age = user.age
+            users[i].student_id = user.student_id
+            return users[i]
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+
+@app.delete("/api/users/{user_id}", status_code=204)
+def delete_user(user_id: int):
+    #Scans each user object in list
+    for u in users:
+        if(u.user_id == user_id):
+            #Upon matching id passed with user object id, the user is removed
             users.remove(u)
             return
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-
-@app.get("/health")
-def health():
-    return {"status": "ok"}
